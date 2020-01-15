81bd03780fd49546acccd61147b1aad61a73af82
zhangxiexia <zhangxiexia@snqu.com>
2020-01-15 16:49:51 +0800

    fix  前置页泡泡水管动画bug

--- src/pages/pc/guide/components/PipeBubble.vue
+++ src/pages/pc/guide/components/PipeBubble.vue
@@ -6,9 +6,10 @@
       :viewBox="viewBox"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <template v-for="item in 50">
-        <image :key="item" :href="paopaoUrl" :width="(13 * Math.random()) / 19.2 + 'vw'">
-          <animateMotion :path="path" :begin="12000 * Math.random() + 'ms'" dur="12s" repeatCount="indefinite" />
+      <template v-for="(item, index) in 50">
+        <image :key="item" :href="paopaoUrl" :width="(13 * Math.random()) / 19.2 + 'vw'" style="opacity: 0">
+          <animate :begin="12000 * randomArr[index] + 'ms'" attributeName="opacity" values="1" />
+          <animateMotion :path="path" :begin="12000 * randomArr[index] + 'ms'" dur="12s" repeatCount="indefinite" />
         </image>
       </template>
     </svg>
@@ -35,8 +36,14 @@ export default {
     return {
       paopaoUrl: require('~/assets/images/guide/paopao_pipe.png'),
       viewBox: `0 0 ${this.width} ${this.height}`,
-      paopao_rate: 1 // 泡泡变化比例
+      paopao_rate: 1, // 泡泡变化比例
+      randomArr: new Array(50).fill(1)
     }
+  },
+  created() {
+    this.randomArr.forEach((item, index) => {
+      this.$set(this.randomArr, index, Math.random())
+    })
   }
 }
 </script>
